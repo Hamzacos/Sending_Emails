ea882ea0086ced05a0db4520e9c2ce4dfe33cbd8
hamzacos <hamza.laqraa@hotmail.com>
2022-08-10 00:18:27 +0100

    Inclure le contenu HTML

--- emailDemo.py
+++ emailDemo.py
@@ -1,19 +1,44 @@
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-port = 587 
-smtp_server = "smtp.gmail.com"
 sender_email = "hamza.laqraa@gmail.com"
 receiver_email = "test@gmail.com"
-password = input("Type your password and press enter:")
-message = """\
-Subject: Hi there
+password = input("Type your password :")
 
-This message is sent from Python."""
+message = MIMEMultipart("alternative")
+message["Subject"] = "multipart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+
+text = """\
+Hi,
+How are you?
+"""
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+      
+    </p>
+  </body>
+</html>
+"""
+
+
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+
+
+message.attach(part1)
+message.attach(part2)
+
 
 context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo() 
-    server.starttls(context=context)
-    server.ehlo()  
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
